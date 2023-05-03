560b32603e7a72739b5481bf4f53ee612e8e0d2d
Raphael Beaudet <raphaelbeaudet@icloud.com>
2023-05-03 14:13:22 +0200

    game board

--- src/components/Morpion.vue
+++ src/components/Morpion.vue
@@ -3,23 +3,35 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const round = ref(1)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="round++">Round #{{ round }}</button>
+  </div>
+  <div class="morpion">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+    .morpion {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    .morpion div {
+        width: 70px;
+        height: 70px;
+        border: 1px solid #333333;
+    }
 </style>
