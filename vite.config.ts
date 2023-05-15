cf07645ffbbb5e80ec4c1a5e0c93cf9e3b752dbb
Raphael Beaudet <raphaelbeaudet@icloud.com>
2023-05-15 09:38:07 +0200

    config-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,24 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
+      //includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        
+        name: 'Morpion',
+        short_name: 'Morpion',
+        description: 'My Awesome TicTac game',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
