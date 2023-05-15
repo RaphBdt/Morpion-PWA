f932d6848b49d3c8e5d00c1beefc45c009c176b6
Raphael Beaudet <raphaelbeaudet@icloud.com>
2023-05-15 09:43:41 +0200

    update pwa file

--- vite.config.ts
+++ vite.config.ts
@@ -11,24 +11,32 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
-      //includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Morpion',
-        short_name: 'Morpion',
-        description: 'My Awesome TicTac game',
-        theme_color: '#ffffff',
-        icons: [
+        "id" : "id",
+        "name": 'Morpion',
+        "short_name": 'Morpion',
+        "start_url" : "/",
+        "description": 'My Awesome TicTac game',
+        "theme_color": '#ffffff',
+        "icons": [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            "src": 'pwa-192x192.png',
+            "sizes": '192x192',
+            "type": 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            "src": 'pwa-512x512.png',
+            "sizes": '512x512',
+            "type": 'image/png'
           }
-        ]
+        ],
+        "background_color" : "#ffffff",
+        "scope" : "/",
+        "lang": "fr",
+        "orientation" : "portrait",
+        "screenshots" : [],
+        "categories" : ["entertainement", "games", "kids"],
       }
     })
   ],
